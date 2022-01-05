f92904653c983e6330fa5b4664d24ec6e63e324f
tcbaby <cbtan@foxmail.com>
2022-01-05 18:19:48 +0800

    fix

--- t_formatShareCode.sh
+++ t_formatShareCode.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
-# 格式化互助码
-# cron 30 22 * * 1,2 t_formatShareCode.sh
+# 互助码
+# cron 30 22 * * 0,1 t_formatShareCode.sh
 
 allMsg=''
+logDir='/ql/log/shufflewzc_faker2_jd_get_share_code/';
+
+cd $logDir
+logFile=`ls -lt | grep '^-' | awk 'NR==1 {print $9}'`
+echo "file: $logFile"
 
 function formatShareCode() {
     local title="************* format $1 $2 **************"
     echo $title
 
-    local m1=`cat /tmp/shareCode.log | awk -F'】' '/京东农场】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/farm \1\n#'`
-    local m2=`cat /tmp/shareCode.log | awk -F'】' '/京东萌宠】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/pet \1\n#'`
-    local m3=`cat /tmp/shareCode.log | awk -F'】' '/种豆得豆】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/bean \1\n#'`
-    local m4=`cat /tmp/shareCode.log | awk -F'】' '/东东工厂】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/ddfactory \1\n#'`
-    local m5=`cat /tmp/shareCode.log | awk -F'】' '/京喜工厂】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/jxfactory \1\n#'`
-    local m6=`cat /tmp/shareCode.log | awk -F'】' '/闪购盲盒】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/sgmh \1\n#'`
+    local m1=`cat $logFile | awk -F'】' '/京东农场】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/farm \1\n#'`
+    local m2=`cat $logFile | awk -F'】' '/京东萌宠】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/pet \1\n#'`
+    local m3=`cat $logFile | awk -F'】' '/种豆得豆】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/bean \1\n#'`
+    local m4=`cat $logFile | awk -F'】' '/东东工厂】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/ddfactory \1\n#'`
+    local m5=`cat $logFile | awk -F'】' '/京喜工厂】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/jxfactory \1\n#'`
+    local m6=`cat $logFile | awk -F'】' '/闪购盲盒】[A-z0-9=]+/ {print $2}' | sed -n "$1,$2p" | tr "\n" "&" | sed -r 's#^(.*)\&$#/sgmh \1\n#'`
 
     local msg="${m1}\n\n${m2}\n\n${m3}\n\n${m4}\n\n${m5}\n\n${m6}"
     allMsg="${allMsg}${title}\n${msg}\n\n\n"
     echo $msg
 }
-
-. /ql/config/env.sh
-node shufflewzc_faker2_jd_get_share_code.js &> /tmp/shareCode.log
 
 total=`echo $JD_COOKIE | awk -F'&' '{print NF}'`
 count=$(($total/5 + 1))
