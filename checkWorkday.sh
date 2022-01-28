b1eba585f72c8ff6dad344d1ae2e37c978a23cdc
tcbaby <cbtan@foxmail.com>
2022-01-28 15:40:30 +0800

    update

--- checkWorkday.sh
+++ checkWorkday.sh
@@ -2,15 +2,16 @@
 # 非工作日 返回状态码 -1
 
 day=`date +%Y%m%d`
+day=20220207
 
 summary=`curl -s https://www.shuyz.com/githubfiles/china-holiday-calender/master/holidayCal.ics \
     | awk -F"BEGIN:VEVENT" -vOFS="\n\n\n" '{ for( i=1; i<=NF; i++) print $i}' \
-    | grep -EA11 "^DTSTART.*:${day}.*" | grep -E 'SUMMARY:' \
-    | awk -F":" '{print $2}'`
+    | grep -EA11 "^DTSTART.*:${day}.*" \
+    | awk -F":" '/SUMMARY:/ {print $2}'`
 
-if [ -n "`echo $summary | grep 假期`" ]; then
-    echo $day $summary
+if [[ $summary =~ .*假期.* ]]; then
+    echo $day 非工作日 $summary
     exit -1
 else
-    echo 工作日 $summary
+    echo $day 工作日 $summary
 fi
