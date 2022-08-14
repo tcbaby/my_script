ec73536f320fc8e917801be0d5f859305e51a453
tcbaby <cbtan@foxmail.com>
2022-08-14 20:50:13 +0800

    update news

--- t_news.js
+++ t_news.js
@@ -12,9 +12,10 @@ const url = 'https://www.163.com/dy/media/T1603594732083.html'
 !(async () => {
     try {
         request(url, async (err, res) => {
-            let list = res.body.match(/<a class="img" href="(.*)">/g)
-            list[0].match(/<a class="img" href="(.*)">/g)
-            news_url = RegExp.$1
+            // let list = res.body.match(/<a class="img" href="(.*)">/g)
+            // list[0].match(/<a class="img" href="(.*)">/g)
+            // news_url = RegExp.$1
+            const news_url = res.body.match(/https:\/\/www.163.com\/dy\/article.*html/g)[1];
 
             request(news_url, async (err, res) => {
                 res.body.match(/<div class="post_body">[^<]*?<p class="f_center">.*?<p id=".*?">.*?<br\/>(.*?)<\/p>/g)[0]
